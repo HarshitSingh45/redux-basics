5534f9fb9f79f97bcde45ae9bbc9ab90437cf3a8
harshitsingh45 <harshitsingh45@gmail.com>
2022-08-22 11:46:05 +0530

    used immer library

--- index.js
+++ index.js
@@ -3,35 +3,62 @@ const redux = require('redux');
 const createStore = redux.createStore;
 
 const ORDERCAKE = 'ORDERCAKE';
+const ORDERICECREAM = 'ORDERICECREAM';
 
-const makeOrder = () => {
+const makeCakeOrder = (qty=1) => {
   return {
     type: ORDERCAKE,
-    qty: 1
+    payload: qty
+  }
+}
+const makeIceCreamOrder = (qty=1) => {
+  return {
+    type: ORDERICECREAM,
+    payload: qty
   }
 }
 
-const initialState = {
+const initialCakeState = {
   qty: 10
 }
+const initialIceCreamState = {
+  qty: 20
+}
 
-const reducer = (state=initialState, action) => {
+const cakeReducer = (state=initialCakeState, action) => {
   switch(action.type) {
     case ORDERCAKE:
       return {
         ...state,
-        qty: state.qty - action.qty
+        qty: state.qty - action.payload
+      }
+    default: return state
+  }
+}
+const icecreamReducer = (state=initialIceCreamState, action) => {
+  switch(action.type) {
+    case ORDERICECREAM:
+      return {
+        ...state,
+        qty: state.qty - action.payload
       }
     default: return state
   }
 }
 
-const store = createStore(reducer);
+const rootReducer = redux.combineReducers({
+  cake: cakeReducer, 
+  icecream: icecreamReducer
+})
+
+const store = createStore(rootReducer);
 console.log('Initial state ', store.getState());
 const unsubscribe = store.subscribe(() => console.log('updated state ', store.getState()))
 
-store.dispatch(makeOrder());
-store.dispatch(makeOrder());
-store.dispatch(makeOrder());
+store.dispatch(makeCakeOrder());
+store.dispatch(makeCakeOrder());
+store.dispatch(makeCakeOrder());
+
+store.dispatch(makeIceCreamOrder());
 
 unsubscribe();
